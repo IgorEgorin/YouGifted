3df75bd6ad555d8072c25bf31981d8233f83ce13
igor <9999999593a@gmail.com>
2018-08-20 18:33:59 +0300

    31072018

--- src/main/java/ru/yougifted/helperpackage/Precondition.java
+++ src/main/java/ru/yougifted/helperpackage/Precondition.java
@@ -3,7 +3,6 @@ package ru.yougifted.helperpackage;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
